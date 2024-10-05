7551a3afce690f9f17ba95f430c9a72eb093d939
firdauz-bk <2302706@sit.singaporetech.edu.sg>
2024-10-05 19:00:09 +0800

    added fake users, delete your carcraft.db

--- dummy_populate_db.py
+++ dummy_populate_db.py
@@ -47,42 +47,11 @@ def insert_wheel_sets():
     cursor.executemany('INSERT INTO wheel_set (name, description) VALUES (?, ?)', wheel_sets)
     conn.commit()
 
-# Function to insert data into the wheel_set table
-def insert_users():
-    # Open CSV file
-    try:
-        df = pd.read_csv("users.csv")
-    except Exception as e:
-        print(e)
-        print("Users.csv not found. Quitting.")
-        return
-
-    # Load first 100 accounts
-    df = df.head(100)
-    for username in df['author']:
-        email = f"{username}@gmail.com"
-        password_hash = generate_password_hash("password")
-        
-        cursor.execute('''
-            INSERT INTO user (username, email, password_hash, admin)
-            VALUES (?, ?, ?, ?)
-            ''', (username, email, password_hash, 0))
-
-    # Insert admin accounts
-    cursor.execute('''
-        INSERT INTO user (username, email, password_hash, admin)
-        VALUES (?, ?, ?, ?)
-        ''', ("admin", "admin@admin.com", generate_password_hash("admin"), 1))
-    
-    conn.commit()
-
-
 # Populate the tables with dummy data
 insert_vehicle_types()
 insert_brands()
 insert_colors()
 insert_wheel_sets()
-insert_users()
 
 print("Dummy data inserted successfully!")
 
---
+++ insert_fake_user.py
@@ -0,0 +1,43 @@
+import sqlite3
+import pandas as pd
+from werkzeug.security import generate_password_hash
+
+# Connect to your SQLite database (adjust the path as necessary)
+conn = sqlite3.connect('carcraft.db')
+cursor = conn.cursor()
+
+# Function to insert data into the wheel_set table
+def insert_users():
+    # Open CSV file
+    try:
+        df = pd.read_csv("users.csv")
+    except Exception as e:
+        print(e)
+        print("Users.csv not found. Quitting.")
+        return
+
+    # Load first 100 accounts
+    df = df.head(100)
+    for username in df['author']:
+        email = f"{username}@gmail.com"
+        password_hash = generate_password_hash("password")
+        
+        cursor.execute('''
+            INSERT INTO user (username, email, password_hash, admin)
+            VALUES (?, ?, ?, ?)
+            ''', (username, email, password_hash, 0))
+
+    # Insert admin accounts
+    cursor.execute('''
+        INSERT INTO user (username, email, password_hash, admin)
+        VALUES (?, ?, ?, ?)
+        ''', ("admin", "admin@admin.com", generate_password_hash("admin"), 1))
+    
+    conn.commit()
+
+insert_users()
+
+print("User data successfully inserted!")
+
+# Close the database connection
+conn.close()
