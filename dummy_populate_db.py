6eaa5c330b2432ddf52e46701d9932ea4c0a2d0b
dragonstonehafiz <dragonstonehafiz@gmail.com>
2024-10-11 13:19:32 +0800

    updated dummy_populate_db to add posts using models with pictures

--- dummy_populate_db.py
+++ dummy_populate_db.py
@@ -1,7 +1,6 @@
 import sqlite3
 from createtables import regenerate_tables
 from insert_fake_user import insert_users
-from insert_models import insert_models
 from insert_customizations import insert_customizations
 from insert_posts_and_comments import insert_posts_and_comments
 
@@ -9,22 +8,31 @@ from insert_posts_and_comments import insert_posts_and_comments
 # Define dummy data for each table
 vehicle_types = [
     ('Sedan',),
-    ('SUV',)
+    ('SUV',),
+    ('Sports',)
 ]
 
 brands = [
     ('Toyota',),
-    ('Honda',)
+    ('Honda',),
+    ('BMW',)
+]
+
+models = [
+    ('Civic', 'Honda', 'Sports',),
+    ('GR86', 'Toyota', 'Sports',),
+    ('M5', 'BMW', 'Sedan',),
+    ('Prius', 'Toyota', 'SUV',)
 ]
 
 colors = [
-    ('Red',),
-    ('Blue',)
+    ('Blue',),
+    ('White',)
 ]
 
 wheel_sets = [
-    ('Alloy Wheels', 'Lightweight and stylish alloy wheels.'),
-    ('Steel Wheels', 'Durable and budget-friendly steel wheels.')
+    ('Alloy', 'Lightweight and stylish alloy wheels.',),
+    ('Steel', 'Durable and budget-friendly steel wheels.',)
 ]
 
 
@@ -48,6 +56,33 @@ def insert_wheel_sets():
     cursor.executemany('INSERT INTO wheel_set (name, description) VALUES (?, ?)', wheel_sets)
     conn.commit()
 
+def insert_models():
+    for model in models:
+        cursor.execute(f"""
+                        SELECT brand_id 
+                        FROM brand
+                        WHERE name = '{model[1]}'
+                        """)
+        brand_id = cursor.fetchone()[0]
+        
+        cursor.execute(f"""
+                        SELECT type_id 
+                        FROM vehicle_type
+                        WHERE name = '{model[2]}'
+                        """)
+        type_id = cursor.fetchone()[0]
+        
+        name = model[0]
+        cursor.execute("""
+                       INSERT INTO model (name, brand_id, type_id)
+                       VALUES (?, ?, ?)
+                       """, 
+                       (name, brand_id, type_id))
+        
+        conn.commit()
+        
+
+
 if __name__ == "__main__":
     # Connect to your SQLite database (adjust the path as necessary)
     conn = sqlite3.connect('carcraft.db')
@@ -61,11 +96,12 @@ if __name__ == "__main__":
     insert_brands()
     insert_colors()
     insert_wheel_sets()
+    insert_models()
+
 
     print("Dummy data inserted successfully!")
 
     insert_users()
-    insert_models()
     insert_customizations()
     insert_posts_and_comments()
 
